4c8f014b93c02d3b87a195f591d676f22cabc723
Errica <errica.cheng@gmail.com>
2016-10-05 19:45:08 -0400

    Test
    Test on Github

--- Group E/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
+++ Group E/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
@@ -6,8 +6,24 @@ Program Task: drive the robot forward for a few seconds
 Feel free to view the example program within the same folder if you need help!
 */
 
+#include<Servo.h>
+#include<Drive.h>
+
+#define PIN_SERVO_RIGHT   9
+#define PIN_SERVO_LEFT    10
+
+int led = 5;
+
+int delayValue = 1000;
+Drive *drive;
+
 void setup(){
-  //your code goes here
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+  drive = new Drive(PIN_SERVO_LEFT, PIN_SERVO_RIGHT); 
+  drive->fwd(50);
+  delay(delayValue);
+  drive->stop();
 }
 
 void loop(){
