ffceebee52f8cb3cb535953a833fec0e36625f0b
Michaela <michaelalizzy@gmail.com>
2016-10-05 20:07:59 -0400

    I completed the lesson and made yellow fade.
    Fades the leds and loops it .

--- Group C/Lessons/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
+++ Group C/Lessons/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
@@ -5,12 +5,24 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Fade in and out the led on pin 5 using 2 for loops
 Feel free to view the example program within the same folder if you need help!
 */
+int red = 3;
+
+int blinkDelay = 20;
 
 void setup(){
-  //your code goes here
-}
-
+  pinMode(red, OUTPUT);
+}  
 void loop(){
-  //also here
-}
+  for(int i =0; i < 255; i += 5){
+    analogWrite(red, i);
+    delay(blinkDelay);
+  }
+  for(int i = 255; i >= 0; i -= 5){
+   analogWrite(red, i);
+   delay(blinkDelay);   
+  }
+  
+ }
+  
+  
 
