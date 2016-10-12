64a8f465954d16a7d2fa2aeed73c8f94b0a42e56
Errica <errica.cheng@gmail.com>
2016-10-12 19:37:13 -0400

    Revert "Buttons Suck"
    This reverts commit 05d4a9eda5b22086aca834437345afd4217a7516.

--- Group B/Lessons/2.1_2.2/2.1_Toggle_an_LED/User_Program2_1/User_Program2_1/User_Program2_1.ino
+++ Group B/Lessons/2.1_2.2/2.1_Toggle_an_LED/User_Program2_1/User_Program2_1/User_Program2_1.ino
@@ -6,43 +6,14 @@ Program Task: Have the LED on pin 5 turn on when the button is pressed
 Feel free to view the example program within the same folder if you need help!
 */
 
-#include <Led.h>
-#include <Button.h>
 
-#define PIN_LEAD_YELLOW  3
-#define PIN_LEAD_RED     5
-#define PIN_LEAD_GREEN   6
-#define PIN_BUTTON       7
-
-Led *yellowLed;
-Led *redLed;
-Led *greenLed;
-Button *aButton;
 
 void setup(){
-  yellowLed = new Led(PIN_LEAD_YELLOW);
-  redLed = new Led(PIN_LEAD_RED);
-  greenLed = new Led(PIN_LEAD_GREEN);
-  aButton = new Button(PIN_BUTTON);
+  //your code goes here
+  
 }
 
 void loop(){
-  if(aButton->isPressed()){
-    redLed->turnOn();
-  }
+  //also here
 }
- /* }
-  else if {
-   redLed->turnOff();
-   yellowLed->turnOn();
-  }
-   else if {
-   yellowLed->turnOff();
-   greenLed->turnOn();
-  }
-  else {
-    greenLed->turnOff();
-  }
-}
-}
-*/
+
