ea3f088700f3f6e6a7ea5bb779559eb74f0fe52b
Xandeh <xanderhopkins@hotmail.com>
2016-10-12 20:06:40 -0400

    Revert "The light fades..."
    This reverts commit aaf819506cae35f8e1e2453d2b0b3deafea3a0d2.

--- Group D/Lessons/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
+++ Group D/Lessons/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
@@ -5,20 +5,12 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Fade in and out the led on pin 5 using 2 for loops
 Feel free to view the example program within the same folder if you need help!
 */
-int led = 5;
-int changeDelay = 10;
+
 void setup(){
-  pinMode (led, OUTPUT);
+  //your code goes here
 }
 
 void loop(){
-  for(int i = 0; i < 255; i += 5){
-    analogWrite (led, i);
-    delay (changeDelay);
-  }
-  for(int i = 255; i >= 0; i -= 5){
-    analogWrite (led, i);
-    delay (changeDelay);
-  }
+  //also here
 }
 
