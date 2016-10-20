cd97934dd289977bff5fd102570981a3c64e1be9
Errica <errica.cheng@gmail.com>
2016-10-19 20:30:09 -0400

    Just Starting This lesson
    10.19.2016

--- Group B/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
+++ Group B/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
@@ -5,11 +5,34 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: drive the robot forward for a few seconds
 Feel free to view the example program within the same folder if you need help!
 */
+#include "Servo.h"
+#include "Drive.h"
+#include "Button.h"
+
+
+#define PIN_SERVO_RIGHT   9
+#define PIN_SERVO_LEFT    10
+#define PUSH_BUTTON   7
+
+int delayValue = 1000;
+Drive *drive;
+Button *aButton;
 
 void setup(){
-  //your code goes here
-}
+ drive = new Drive (PIN_SERVO_LEFT, PIN_SERVO_RIGHT)
+ 
 
 void loop(){
-  //also here
+  if (aButton->isPressed()){
+    drive->fwd(100);
+   else drive->stop();
+  }
 }
+ /*if(aButton->isPressed()){
+    redLed->setOn(true);
+  } else {
+    redLed->setOn(false);
+  }
+}
+*/
+
