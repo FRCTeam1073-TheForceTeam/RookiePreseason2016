4bd6579d2a8964902b66934ece11cc75071d6ffe
Errica <errica.cheng@gmail.com>
2016-10-26 20:04:34 -0400

    It moves!!
    Forward!!!

--- Group B/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
+++ Group B/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
@@ -6,10 +6,27 @@ Program Task: drive the robot forward for a few seconds
 Feel free to view the example program within the same folder if you need help!
 */
 
-void setup(){
-  //your code goes here
+int val = 0;
+
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
 }
 
-void loop(){
-  //also here
+void loop() {
+  /*if (Serial.available() > 0) {
+    String str = "";
+    str += ((char) Serial.read());
+    
+    val += str.toInt(;
+  Serial.write(val); */ 
+  //}
+  analogWrite(9, 40);
+  analogWrite(10, 215);
 }
--- Group C/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
+++ Group C/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
@@ -1,3 +1,5 @@
+#include <Drive.h>
+
 /*Software 1073 Arduino BoeBots
 User Program 
 Method stubs are pre-made. add your own code to complete the challenge!
@@ -5,11 +7,20 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: drive the robot forward for a few seconds
 Feel free to view the example program within the same folder if you need help!
 */
+#define PIN_SERVO_RIGHT 10
+#define PIN_SERVO_LEFT 9
 
-void setup(){
-  //your code goes here
+int delayValue = 3000;
+
+Drive *drive;
+ void setup(){
+  drive = new Drive(PIN_SERVO_LEFT, PIN_SERVO_RIGHT);
+  drive->fwd(50);
+  delay(delayValue);
+  drive->stop();
+ 
 }
 
 void loop(){
-  //also here
+  
 }
