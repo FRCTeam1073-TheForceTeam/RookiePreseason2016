3f84b7819269f605b052687766d3552454d5d0d2
Xandeh <xanderhopkins@hotmail.com>
2016-10-26 20:26:42 -0400

    Potent Potentiometer Potential Pt 1
    Potentiometer puns aside, we learned how to use the weird red spinny
    knob!
    Nothing special in the code, really. Just us.
    We're special.

--- Group D/Lessons/2.1_2.2/2.2_Dim_an_LED_pot/User_Program2_2/User_Program2_2.ino
+++ Group D/Lessons/2.1_2.2/2.2_Dim_an_LED_pot/User_Program2_2/User_Program2_2.ino
@@ -5,12 +5,18 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Dim the led on pin 5 using the potentiometer, so that as the potentiometer is turned, the brightness is adjusted
 Feel free to view the example program within the same folder if you need help!
 */
-
+#include "Potentiometer.h"
+int ledNum = 5;
+int potentiometerNum = 5;
+int potBrightness = 0;
+int finalBrightness = 0;
 void setup(){
-  //your code goes here
+pinMode(ledNum, OUTPUT);
 }
 
 void loop(){
-  //also here
+potBrightness = analogRead(5);
+finalBrightness = map(potBrightness, 0, 1023, 0, 255);
+analogWrite(ledNum, finalBrightness);
 }
 
